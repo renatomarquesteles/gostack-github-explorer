0fa6c73d2928cbad218daf265f89b0a251b87b7c
Renato <renatomarquesteles@gmail.com>
2020-05-21 05:03:08 -0300

    estilizando detalhes do repo

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,53 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repositório: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/37571156?s=460&u=7f524951cfc164e30ee017e5fa6b3dc98144da1e&v=4"
+            alt="Renato"
+          />
+          <div>
+            <strong>NomeDoRepositorio</strong>
+            <p>Descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>9999</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>9999</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>999</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>Issue</strong>
+            <p>Descriçao</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
