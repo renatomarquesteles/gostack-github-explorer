8fbb1555fedef1b81fe7bec7b26792708e7e3670
Renato <renatomarquesteles@gmail.com>
2020-05-21 04:27:08 -0300

    lidando com erros

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,11 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { shade } from 'polished';
 
-const expandCard = keyframes`
+interface FormProps {
+  hasError: boolean;
+}
+
+const fadeIn = keyframes`
   0% { opacity: 0 }
   100% { opacity: 1 }
 `;
@@ -14,7 +18,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -26,6 +30,14 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 1px solid #fff;
+    transition: border-color 0.3s;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -48,6 +60,13 @@ export const Form = styled.form`
   }
 `;
 
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+  animation: ${fadeIn} 0.3s;
+`;
+
 export const Repositories = styled.div`
   margin-top: 80px;
   max-width: 700px;
@@ -61,7 +80,7 @@ export const Repositories = styled.div`
     display: flex;
     align-items: center;
     transition: transform 0.2s;
-    animation: ${expandCard} 0.8s;
+    animation: ${fadeIn} 0.8s;
 
     & + a {
       margin-top: 16px;
